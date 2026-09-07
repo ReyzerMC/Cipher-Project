767c74a10bb73479be8164c675d2b0265e67fda1
ReyzerMC <libertoborre@gmail.com>
2026-09-08 01:01:14 +0200

    a

--- src/components/changelog.tsx
+++ src/components/changelog.tsx
@@ -2,7 +2,7 @@ import type { Log } from "../types/hsr";
 
 const v4_5: Log = {
     version: "4.5",
-    title: "lo q sea",
+    title: "To Roll the Stars in Astropolis",
     changes: [
         `Added all character up to v4.5`,
         `Added all lightcones up to v4.5`
