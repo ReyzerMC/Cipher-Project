84278a051d1656b2d57ca6be322332fc6dad4909
ReyzerMC <libertoborre@gmail.com>
2026-09-08 14:56:58 +0200

    fixed changelog

--- src/components/changelog.tsx
+++ src/components/changelog.tsx
@@ -10,7 +10,7 @@ const v4_5: Log = {
 }
 
 const v4_5_2: Log = {
-    version: "4.5",
+    version: "4.5 / 2",
     title: "To Roll the Stars in Astropolis / 2",
     changes: [
         `Added Pearl Beta Character`
