2a9d8f8f5adfd2afe5345f1290794cf4e600b61e
ReyzerMC <libertoborre@gmail.com>
2026-09-22 17:33:31 +0200

    redirects

---
+++ src/Introsection.tsx
@@ -0,0 +1,24 @@
+export default function IntroSection() {
+  return (
+    <section aria-label="Acerca de Cipher Project">
+      <h1>Cipher Project — </h1>
+
+      <p>
+        Cipher Project is an <strong>Honkai: Star Rail</strong> 
+        Wiki where you can look for characters and other things of the game
+      </p>
+
+      <h2>¿What does the page have?</h2>
+      <p>
+        Now a days Cipher Project has all the characters and lightcones availables Up to 4.5 version of Honkai: Star Rail.
+      </p>
+
+      <h2>Compatibility</h2>
+      <p>
+        The interface is optimized for desktop screens in
+        1920x1080 y 2560x1440, and has support for mobile phones and
+        tablets.
+      </p>
+    </section>
+  );
+}
